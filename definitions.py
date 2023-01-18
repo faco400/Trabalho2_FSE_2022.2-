e627b7d6e1bfee0bf427d935f2252497062ae109
faco400 <vd.souza1999@gmail.com>
2023-01-18 13:08:00 -0300

    Start reading cmds from dashoard

---
+++ definitions.py
@@ -0,0 +1,20 @@
+
+# MODBUS
+ESP32 = b'\x01' # esp32 address
+CODE = [b'\x23', b'\x16'] #solicita(le) / envia(controla)
+matricula = b'\x05\x05\x00\x00' 
+C1 = b'\xC1' # Solicita Temperatura Interna
+C2 = b'\xC2' # Solicita Temperatura de referencia
+C3 = b'\xC3' # Le comandos dashboard
+D1 = b'\xD1' # Envia sinal de controle Int (4 bytes)
+D2 = b'\xD2' # Envia  sinal de Referência Float (4 bytes)
+D3 = b'\xD3' # Envia Estado do Sistema (Ligado = 1 / Desligado = 0)
+D4 = b'\xD4' # Modo de Controle da Temperatura de referência (Dashboard = 0 / Curva/Terminal = 1) (1 byte)
+D5 = b'\xD5' # Envia Estado de Funcionamento (Funcionando = 1 / Parado = 0)
+D6 = b'\xD6' # Envia Temperatura Ambiente (Float))
+sample = b'\x01\x23\xC1\x05\x05\x00\x00'
+sample2 = [0x01,0x23,0xC1,0x05,0x05,0x00,0x00]
+
+#GPIO
+resistor = 23
+ventoinha = 24
--- main.py
+++ main.py
@@ -2,20 +2,14 @@ import RPi.GPIO as GPIO
 import bme.externalTemp as bme
 import serial
 import CRC.crc16 as crc16
+import definitions as defs
+import time
 
+uart0_filestream = -1
 
-resistor = 23
-ventoinha = 24
-matricula = b'\x05\x05\x00\x00' 
-C1 = b'\xC1'
-C2 = b'\xC2'
-C3 = b'\xC3'
-D1 = b'\xD1'
-D2 = b'\xD2'
-D3 = b'\xD3'
-D4 = b'\xD4'
-D5 = b'\xD5'
-D6 = b'\xD6'
+# To Do...
+def init_states():
+  pass
 
 
 def init_UART():
@@ -24,20 +18,19 @@ def init_UART():
     print('"Erro - Não foi possível iniciar a UART.\n"')
   else:
     print("UART inicializada!\n")
+  return uart0_filestream
+
   
-  # testing calc of crc
-  message = bytearray(b'\x01\x23'+C2+matricula)
-  crc = crc16.calcCRC(message,7)
-  print(str(hex(crc)))
-  crc = crc.to_bytes(2,'little')
-  message.extend(crc)
-  print(message)
-  # Exemplo de como mandar comando em python
-  #message = b'\x01\x23'+C1+matricula+crc
-  print("escrevendo...")
-  uart0_filestream.write(message)
-
-
+  # # testing calc of crc
+  # crc = crc16.calcCRC(defs.ESP32+defs.CODE[1]+defs.C1+defs.matricula,7).to_bytes(2,'little')
+  # # Exemplo de como mandar comando em python
+  # message = defs.ESP32+defs.CODE[1]+defs.C1+defs.matricula+crc
+  # print("escrevendo...")
+  # uart0_filestream.write(message)
+  # print(message)
+  # print("lendo...")
+  # resp = uart0_filestream.read(9)
+  # print(resp)
 
 def init_GPIO(pResistor, pVentoinha):
   # rasp pin mode setup
@@ -46,16 +39,25 @@ def init_GPIO(pResistor, pVentoinha):
   GPIO.setup(pResistor,GPIO.OUT)
   GPIO.setup(pVentoinha,GPIO.OUT)
 
+def read_cmd(uart):
+  crc = crc16.calcCRC(defs.ESP32+defs.CODE[1]+defs.C3+defs.matricula,7).to_bytes(2,'little')
+  message = defs.ESP32 + defs.CODE[1] + defs.C3 + defs.matricula + crc
+  uart.write(message)
+  resp = uart.read(9)
+  print('crc')
+  print(resp[-2:]) # imprimendo crc
+  print('cmd')
+  print(resp[3:7]) # imprimindo comando
+
+  time.sleep(0.5)
+
+
 
 if __name__ == "__main__":
-  init_GPIO(resistor,ventoinha)
+  init_GPIO(defs.resistor,defs.ventoinha)
   # bme.init_I2C()
-  init_UART()
-
-"""
-0x01
-0x23
-0xC1 N N N N
-Solicita Temperatura Interna
-0x00 0x23 0xC1 + float (4 bytes)
-"""
+  uart = init_UART()
+  
+  #Loop principal
+  while 1:
+    op = read_cmd(uart)
